1f847e7538e7fee9eb7294a22aa424b72bfbd9f0
Mist <mistlawxi@gmail.com>
2023-01-21 19:06:16 +0500

    Attached inputs to camera

--- PhysicsSim/Camera.js
+++ PhysicsSim/Camera.js
@@ -1,18 +1,37 @@
 class Camera{
     constructor(x, y, screen_width, screen_height){
         this.pos = createVector(x, y)
-        this.screen = createVector(-screen_width/2, -screen_height/2)
+        this.screen = createVector(screen_width/2, screen_height/2)
     }
 
-    draw_and_update(physicsObjectList){
+    draw_and_update(physicsObjectList, dt){
+        this.move();
         for(var i = 0; i < physicsObjectList.length; i++){
-            physicsObj = physicsObjectList[i]
-            var translated_coords = translate(physicsObj.pos)
-            physicsObj.draw(translated_coord)
+            var physicsObj = physicsObjectList[i]
+            physicsObj.update(physicsObjectList, dt)
+            var translated_coords = this.translate(physicsObj.pos)
+            physicsObj.draw(translated_coords)
         }
     }
 
     translate(position){
-        return p5.Vector.add(this.pos, p5.Vector.add(this.screen, position))
+        var output = p5.Vector.add(this.pos, p5.Vector.add(this.screen, position))
+        return output;
+    }
+
+    move(){
+        var speed = 5;
+        if(inputs['a'] == true){
+            this.pos.x += speed
+        }
+        if(inputs['s'] == true){
+            this.pos.y -= speed
+        }
+        if(inputs['d'] == true){
+            this.pos.x -= speed
+        }
+        if(inputs['w'] == true){
+            this.pos.y += speed
+        }
     }
 }
--- PhysicsSim/sketch.js
+++ PhysicsSim/sketch.js
@@ -7,26 +7,18 @@ function setup() {
   createCanvas(800, 600);
   frameRate(60);
 
-  camera = Camera(0, 0, width, height);
+  camera = new Camera(0, 0, width, height);
 
-  physicsObjectList.push(new PhysicsObj(100,100, 20, 5))
-  physicsObjectList.push(new PhysicsObj(200,200, 20, 5))
-  physicsObjectList.push(new PhysicsObj(300,200, 20, 5))
-  physicsObjectList.push(new PhysicsObj(400,200, 20, 5))
+  physicsObjectList.push(new PhysicsObj(0, 0, 20, 5))
+  physicsObjectList.push(new PhysicsObj(200,100, 20, 5))
+  physicsObjectList.push(new PhysicsObj(-100,20, 20, 5))
+  physicsObjectList.push(new PhysicsObj(100,200, 20, 5))
   
 }
 
 function draw() {
   // put drawing code here
   background(255)
-  camera.draw_and_update(physicsObjectList);
-  
-  // for(var i = 0; i < physicsObjectList.length; i++){
-  //   if(i==0){
-  //     physicsObjectList[i].printProperties()
-  //   }
-  //   physicsObjectList[i].update(physicsObjectList, deltaTime)
-  //   physicsObjectList[i].draw()
-  // }
+  camera.draw_and_update(physicsObjectList, deltaTime);
 }
 
